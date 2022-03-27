116edb24f162614afb2851bb64f255563f3e46e1
aguzman <abel.aguzmans@gmail.com>
2022-03-27 15:51:14 +0800

    master; go.mod go.sum main.go plugins/glacier/glacier.go

--- main.go
+++ main.go
@@ -1,22 +1,29 @@
 package main
 
 import (
-    "runtime"
-    "github.com/gin-gonic/gin"
-    "github.com/aguzmans/cool-storage/plugins/glacier"
+	// "main/plugins/glacier"
+	"runtime"
+
+	"github.com/aguzmans/cool-storage/plugins/glacier"
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
-    Glacier()
-    router := gin.Default()
-    router.GET("/api/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "pong",
-        })
-    })
+func coolStorage() {
+	// x := new.Glacier()
+	glacier.Glacier()
+	router := gin.Default()
+	router.GET("/api/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 
-    router.GET("/os", func(c *gin.Context) {
-        c.String(200, runtime.GOOS)
-    })
-    router.Run(":5000")
+	router.GET("/os", func(c *gin.Context) {
+		c.String(200, runtime.GOOS)
+	})
+	router.Run(":5000")
+}
+
+func main() {
+	coolStorage()
 }
--- plugins/glacier/glacier.go
+++ plugins/glacier/glacier.go
@@ -1,7 +1,49 @@
 package glacier
 
-import "fmt"
+import (
+	// "fmt"
 
-func Glacier(){
-	fmt.Println("XXX")
+	// "context"
+
+	// "github.com/aws/aws-sdk-go-v2/config"
+	// "github.com/aws/aws-sdk-go/aws"
+	// "github.com/aws/aws-sdk-go/aws/credentials"
+	// "github.com/aws/aws-sdk-go/aws/session"
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+)
+
+const (
+	S3_REGION = "ap-southeast-1"
+)
+
+func Glacier() {
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithSharedConfigProfile("nihao-prod"))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Panicln(cfg)
+	// create an AWS session which can be
+	// reused if we're uploading many files
+	// s, err := session.NewSession(&aws.Config{
+	// 	Region: aws.String(S3_REGION),
+	// 	Credentials: credentials.NewStaticCredentials(
+	// 		"XXX",
+	// 		"YYY",
+	// 		""),
+	// })
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// err = uploadFileToS3(s, "discharge-letter-787653.pdf")
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 }
